8baf91451379b3590da4e97e2d50673f4c3c606b
Harrison Clark <haclark30@gmail.com>
2021-01-07 13:47:29 -0600

    updated tests to use files for mock_api data and expected output; refactored tests

--- test_cli.py
+++ test_cli.py
@@ -3,6 +3,7 @@ import cli
 from habitica import HabiticaAPI
 from unittest.mock import patch, Mock
 from io import StringIO
+import json
 from textwrap import dedent
 
 class TestCLI(unittest.TestCase):
@@ -11,42 +12,6 @@ class TestCLI(unittest.TestCase):
     def setUp(self):
         self.parser = cli.setup_parser()
         self.hbt_api = HabiticaAPI(cli.get_auth())
-
-
-    @patch('habitica.requests.get')
-    def test_status(self, mock_status):
-
-        status = {'profile': {'name':'test user'},
-        'stats': {
-            'lvl' : 1,
-            'class' : "Warrior",
-            'hp' : 10,
-            'maxHealth' : 100,
-            'exp' : 100,
-            'toNextLevel' : 1000,
-            'mp' : 10,
-            'maxMP' : 50 
-        }}
-
-        expected_output = """\
-        test user
-        --------------------
-        Level 1 Warrior
-        --------------------
-        HP: 10/100
-        XP: 100/1000
-        MP: 10/50
-        """
-
-        expected_output = dedent(expected_output)
-
-        mock_status.return_value.status_code = 200
-        mock_status.return_value.json.return_value = {'data': status}
-        args = self.parser.parse_args(["status"])
-
-        with patch('sys.stdout', new=StringIO()) as mock_out:
-            cli.run_command(args, self.hbt_api)
-            self.assertEqual(mock_out.getvalue(), expected_output)
 
     def test_find_matching_tasks(self):
         test_tasks = [{'text':'test1'}, {'text':'test2'}]
@@ -57,6 +22,42 @@ class TestCLI(unittest.TestCase):
         self.assertEqual(match_test_all, test_tasks)
         self.assertEqual([test_tasks[0]], match_test_one)
         self.assertEqual([], match_test_none)
-        
+
+    @patch('habitica.requests.get')
+    def test_status(self, mock_status):
+        args = self.parser.parse_args(["status"])
+        self.mock_get_request(args, mock_status,
+            'mock_data/status.json',
+            'mock_data/expected_output/status.txt')
+
+    @patch('habitica.requests.get')
+    def test_list_all_tasks(self, mock_tasks):
+        args = self.parser.parse_args(["list"])
+        self.mock_get_request(args, mock_tasks, 
+            'mock_data/tasks_all.json',
+            'mock_data/expected_output/tasks_all.txt')
+
+    @patch('habitica.requests.get')
+    def test_list_habit_tasks(self, mock_tasks):
+        args = self.parser.parse_args(["list", "habits"])
+        self.mock_get_request(args, mock_tasks, 'mock_data/tasks_habits.json',
+            'mock_data/expected_output/tasks_habits.txt')
+
+    def mock_get_request(self, args, mock_api, mock_data_path, expected_output_path):
+        mock_api.return_value.status_code = 200
+
+        with open(mock_data_path) as f:
+            data = json.load(f)
+
+        with open(expected_output_path) as f:
+            expected_output = f.read()
+
+        mock_api.return_value.json.return_value = data
+
+        with patch('sys.stdout', new=StringIO()) as mock_out:
+            cli.run_command(args, self.hbt_api)
+            self.assertEqual(mock_out.getvalue(), expected_output)
+
+
 if __name__ == "__main__":
     unittest.main()
